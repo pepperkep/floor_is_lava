9cb841055c88d96e69d44df57fbe8840aa1042ed
Austin Keppers <akeppers21@gmail.com>
2019-04-06 18:57:46 -0400

    Added changing level objectives

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+
+    [SerializeField] private ObjectivePoint[] objectiveList;
+    private int currentObjective = 0;
+
+    public void BeginLevel(){
+        Debug.Log("Level has begun!");
+    }
+
+    public void EndLevel(){
+        Debug.Log("Level has ended!");
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectiveList[currentObjective].IsActive = true;
+        BeginLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!objectiveList[currentObjective].IsActive){
+            if(currentObjective < objectiveList.Length - 1){
+                currentObjective++;
+                objectiveList[currentObjective].IsActive = true;
+            }
+            else
+                EndLevel();
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectivePoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectivePoint : MonoBehaviour
+{
+
+    private bool isActive = false;
+
+    public bool IsActive{
+        get => isActive;
+        set{
+            if(value)
+                activeParticles.Play();
+            else
+                activeParticles.Stop();
+            isActive = value;
+        }
+    }
+    
+    private ParticleSystem activeParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeParticles = GetComponent<ParticleSystem>();  
+        activeParticles.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collision){
+        if(collision.gameObject.name == "Player")
+            IsActive = false;
+    }
+}
