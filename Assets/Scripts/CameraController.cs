9b226d63c1dfa7c9800021e5df27b34aead9d7a8
Trey Starshak <threestarhouse@gmail.com>
2019-04-06 20:58:03 -0400

    Implement basic Drag+Drop Script and Camera Switching script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera PlayCamera;
+    public Camera DragCamera;
+
+    bool editMode;
+
+    public void ShowEditMode()
+    {
+        editMode = true;
+        PlayCamera.enabled = false;
+        DragCamera.enabled = true;
+    }
+
+    public void ShowPlatformMode()
+    {
+        editMode = false;
+        PlayCamera.enabled = true;
+        DragCamera.enabled = false;
+    }
+
+    void Start()
+    {
+        ShowPlatformMode();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (editMode)
+            {
+                ShowPlatformMode();
+            }
+            else
+                ShowEditMode();
+        }
+    }
+}
