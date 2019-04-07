8bdfc8aed6601624cf4a1d0478a709c67c3bf0c3
Austin Keppers <akeppers21@gmail.com>
2019-04-07 16:09:13 -0400

    Added balloons and fixed particles

--- Assets/Scripts/Furniture.cs
+++ Assets/Scripts/Furniture.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class Furniture : MonoBehaviour
 {
+    [SerializeField] private int numberBalloons;
+
+    public int NumberBallons{
+        get => numberBalloons;
+        set{
+            risingHeight = originalHeight + value * heightBalloonMultiplier;
+            numberBalloons = value;
+        }
+    }
+
+    private float originalHeight;
+    private float risingHeight;
+    private float heightBalloonMultiplier = 0.8f;
+    private float risingVelocity = 4f;
+    private float loweringVelocity = 15f;
+
+    void Awake(){
+        originalHeight = transform.position.y;
+        risingHeight = originalHeight + numberBalloons * heightBalloonMultiplier;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +34,25 @@ public class Furniture : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void FixedUpdate(){
+        risingHeight = originalHeight + numberBalloons * heightBalloonMultiplier;
+        if(transform.position.y < risingHeight){
+            float newHeight = transform.position.y + risingVelocity * Time.fixedDeltaTime;
+            if(newHeight > risingHeight)
+                transform.position = new Vector2(transform.position.x, risingHeight);
+            else
+                transform.position = new Vector2(transform.position.x, newHeight);
+        }
+        if(transform.position.y > risingHeight){
+            float newHeight = transform.position.y - loweringVelocity * Time.fixedDeltaTime;
+            if(newHeight < risingHeight)
+                transform.position = new Vector2(transform.position.x, risingHeight);
+            else
+                transform.position = new Vector2(transform.position.x, newHeight);
+        }
         
     }
 }
