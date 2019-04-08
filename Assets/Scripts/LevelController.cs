887a0ee19752a3c1ea6de2612227c447a24bede9
Austin Keppers <akeppers21@gmail.com>
2019-04-08 15:51:27 -0400

    Added changing lava level

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -11,6 +11,7 @@ public class LevelController : MonoBehaviour
 
     private int currentObjective = 0;
     private CameraController modeSwitch;
+    private bool lavaSwitch = false;
     public  GameObject lava;
     public  GameObject floor;
     public GameObject player;
@@ -18,7 +19,8 @@ public class LevelController : MonoBehaviour
     public static float floorHeight;
     public static Vector2 floorPosition;
     public SpriteRenderer floorSprite;
-     public Sprite lavaSprite;
+    public Sprite lavaSprite;
+    public float lavaSizeMultiplier;
    
    public void BeginLevel(){
         Debug.Log("Level has begun!");
@@ -59,16 +61,21 @@ public class LevelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(modeSwitch.dragMode == false){
+        if(!modeSwitch.dragMode && !lavaSwitch){
             lava.SetActive(true);
             lava.transform.position = floor.transform.position;
             WaterArea lavaArea = lava.GetComponent<WaterArea>();
             lavaArea.size = new Vector2(floor.transform.localScale.x, floor.transform.localScale.y);
+            lavaArea.AdjustComponentSizes();
             floor.SetActive(false);
+            lavaSwitch = true;  
         }
         if(!objectiveList[currentObjective].IsActive){
             if (currentObjective < objectiveList.Length - 1)
             {
+                WaterArea lavaArea = lava.GetComponent<WaterArea>();
+                lavaArea.size = new Vector2(lavaArea.size.x, lavaArea.size.y * lavaSizeMultiplier);
+                lavaArea.AdjustComponentSizes();
                 currentObjective++;
                 objectiveList[currentObjective].IsActive = true;
             }
